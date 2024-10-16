59194a5708ad133f069d8b336f85043d8c541ea6
HEGGRIA <bshengtao@gmail.com>
2024-10-16 17:04:33 +0800

    feat: new post

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -34,6 +34,7 @@ declare module 'vue-router/auto-routes' {
     '/posts/react-fiber': RouteRecordInfo<'/posts/react-fiber', '/posts/react-fiber', Record<never, never>, Record<never, never>>,
     '/posts/react-hook-form-type': RouteRecordInfo<'/posts/react-hook-form-type', '/posts/react-hook-form-type', Record<never, never>, Record<never, never>>,
     '/posts/tankstank-query': RouteRecordInfo<'/posts/tankstank-query', '/posts/tankstank-query', Record<never, never>, Record<never, never>>,
+    '/posts/vue-reactive-1': RouteRecordInfo<'/posts/vue-reactive-1', '/posts/vue-reactive-1', Record<never, never>, Record<never, never>>,
     '/posts/web-render-types': RouteRecordInfo<'/posts/web-render-types', '/posts/web-render-types', Record<never, never>, Record<never, never>>,
   }
 }
