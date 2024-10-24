93747ce45114421811ab5c80e9ad1267fae862c6
HEGGRIA <bshengtao@gmail.com>
2024-10-24 17:58:41 +0800

    feat: new post

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -28,6 +28,8 @@ declare module 'vue-router/auto-routes' {
     '/posts/a-note': RouteRecordInfo<'/posts/a-note', '/posts/a-note', Record<never, never>, Record<never, never>>,
     '/posts/component-library-analysis': RouteRecordInfo<'/posts/component-library-analysis', '/posts/component-library-analysis', Record<never, never>, Record<never, never>>,
     '/posts/first-post': RouteRecordInfo<'/posts/first-post', '/posts/first-post', Record<never, never>, Record<never, never>>,
+    '/posts/L3.5-planning': RouteRecordInfo<'/posts/L3.5-planning', '/posts/L3/5-planning', Record<never, never>, Record<never, never>>,
+    '/posts/leetcode-1': RouteRecordInfo<'/posts/leetcode-1', '/posts/leetcode-1', Record<never, never>, Record<never, never>>,
     '/posts/mac-config': RouteRecordInfo<'/posts/mac-config', '/posts/mac-config', Record<never, never>, Record<never, never>>,
     '/posts/package-manager': RouteRecordInfo<'/posts/package-manager', '/posts/package-manager', Record<never, never>, Record<never, never>>,
     '/posts/react-event': RouteRecordInfo<'/posts/react-event', '/posts/react-event', Record<never, never>, Record<never, never>>,
